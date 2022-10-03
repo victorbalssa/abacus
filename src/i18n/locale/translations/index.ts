307c17126390c0c35e55ce8a177864c64757bf53
victorbalssa <git@victorbalssa.ca>
2022-10-03 14:50:15 -0400

    [ADD] French translation.

--- src/i18n/locale/translations/index.ts
+++ src/i18n/locale/translations/index.ts
@@ -1,8 +1,12 @@
 import enUS from './en-US';
 import ptBR from './pt-BR';
+import frFR from './fr-FR';
 
 export default {
   en: enUS,
+  fr: frFR,
+  pt: ptBR,
   'en-US': enUS,
   'pt-BR': ptBR,
+  'fr-FR': frFR,
 };
