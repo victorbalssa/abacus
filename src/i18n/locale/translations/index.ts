80aec36e0cd770dbcda58e9f725273b7f712c154
umarhadi <hi@umarhadi.dev>
2022-10-10 14:44:06 +0700

    add Bahasa Indonesia translation
    Signed-off-by: umarhadi <hi@umarhadi.dev>

--- src/i18n/locale/translations/index.ts
+++ src/i18n/locale/translations/index.ts
@@ -1,12 +1,15 @@
 import enUS from './en-US';
 import ptBR from './pt-BR';
 import frFR from './fr-FR';
+import idID from './id-ID';
 
 export default {
   en: enUS,
   fr: frFR,
   pt: ptBR,
+  id: idID,
   'en-US': enUS,
   'pt-BR': ptBR,
   'fr-FR': frFR,
+  'id-ID': idID,
 };
