718538119ccfc75aa2ab2eb5f91e357d4cb95230
Guillem Arias Fauste <accounts@gariasf.com>
2022-10-12 23:01:42 +0200

    Declare Spanish in translations index

--- src/i18n/locale/translations/index.ts
+++ src/i18n/locale/translations/index.ts
@@ -1,12 +1,15 @@
 import enUS from './en-US';
 import ptBR from './pt-BR';
 import frFR from './fr-FR';
+import esES from './es-ES';
 
 export default {
   en: enUS,
   fr: frFR,
   pt: ptBR,
+  es: esES,
   'en-US': enUS,
   'pt-BR': ptBR,
   'fr-FR': frFR,
+  'es-ES': esEs,
 };
