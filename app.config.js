a1889184c288fc3d0ceb5d535afc5f1fc44efb79
victorbalssa <github@victorbalssa.ca>
2024-05-05 19:27:58 -0400

    v0.15.0

--- app.config.js
+++ app.config.js
@@ -9,7 +9,7 @@ export default {
     'ios',
     'android',
   ],
-  version: '0.14.0',
+  version: '0.15.0',
   orientation: 'portrait',
   updates: {
     enabled: true,
@@ -38,7 +38,7 @@ export default {
       usesNonExemptEncryption: false,
     },
     bundleIdentifier: IS_DEV ? 'abacus.fireflyiii.ios.app.dev' : 'abacus.ios.app',
-    buildNumber: '0.14.0',
+    buildNumber: '0.15.0',
   },
   android: {
     icon: './src/images/icon-abacus.png',
@@ -56,7 +56,7 @@ export default {
     },
     playStoreUrl: 'https://play.google.com/store/apps/details?id=abacus.fireflyiii.android.app',
     package: IS_DEV ? 'abacus.fireflyiii.android.app.dev' : 'abacus.fireflyiii.android.app',
-    versionCode: 22,
+    versionCode: 23,
   },
   scheme: 'abacusfiiiapp',
   githubUrl: 'https://github.com/victorbalssa/abacus',
