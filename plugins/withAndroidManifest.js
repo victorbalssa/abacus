4a75fc5161a96c9fa62d2f0566ebe40f46b49b70
hhhuut <hhhuut@users.noreply.github.com>
2026-09-01 08:27:55 +0200

    fix: set android network security config to trust user CAs

--- plugins/withAndroidManifest.js
+++ plugins/withAndroidManifest.js
@@ -1,23 +1,52 @@
-const { withAndroidManifest } = require('expo/config-plugins');
+const { withAndroidManifest, withDangerousMod } = require('expo/config-plugins');
+const { writeFileSync, mkdirSync } = require('fs');
+const { resolve } = require('path');
 
-function removeNetworkSecurityConfig(androidManifest) {
+function addNetworkSecurityConfig(androidManifest) {
   const newManifest = { ...androidManifest };
 
-  newManifest.manifest.application[0].$['android:networkSecurityConfig'] = undefined;
+  newManifest.manifest.application[0].$['android:networkSecurityConfig'] = '@xml/network_security_config';
   newManifest.manifest.application[0].$['android:usesCleartextTraffic'] = 'true';
 
   return newManifest;
 }
 
+function withNetworkSecurityConfigFile(config) {
+  return withDangerousMod(config, [
+    'android',
+    (c) => {
+      const xmlDir = resolve(c.modRequest.platformProjectRoot, 'app/src/main/res/xml');
+      mkdirSync(xmlDir, { recursive: true });
+      writeFileSync(
+        resolve(xmlDir, 'network_security_config.xml'),
+        `<?xml version="1.0" encoding="utf-8"?>
+<network-security-config>
+    <base-config cleartextTrafficPermitted="true">
+        <trust-anchors>
+            <certificates src="system" />
+            <certificates src="user" />
+        </trust-anchors>
+    </base-config>
+</network-security-config>
+`,
+      );
+      return c;
+    },
+  ]);
+}
+
 module.exports = function withAndroidPlugin(config) {
-  return withAndroidManifest(config, (configWithProps) => {
+  let newConfig = withAndroidManifest(config, (configWithProps) => {
     const mainApplication = configWithProps?.modResults;
 
     if (mainApplication) {
     // eslint-disable-next-line no-param-reassign
-      configWithProps.modResults = removeNetworkSecurityConfig(mainApplication);
+      configWithProps.modResults = addNetworkSecurityConfig(mainApplication);
     }
 
     return configWithProps;
   });
+
+  newConfig = withNetworkSecurityConfigFile(newConfig);
+  return newConfig;
 };
